071d9e718645e33dc4199c7fc5022e38fcf3f63b
Fereshteh Heidari <fereshte121@gmail.com>
2021-04-19 23:22:31 -0700

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import Enzyme, { shallow } from "enzyme";
+import enzymeAdapter from "enzyme-adapter-react-16";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new enzymeAdapter() });
+test("renders learn react link", () => {
+  const wrapper = shallow(<App />);
 });
